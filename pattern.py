a212fd195a05ad5776dff32fbf2a9c7876c819e5
RAHULS09 <84888408+RAHULS09@users.noreply.github.com>
2022-01-20 14:16:29 +0530

    Delete pattern.py

--- pattern.py
+++
@@ -1,9 +0,0 @@
-x=input('Enter the size of pattern:')
-n=int(x)
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        if (i+j)%2==0:
-            print("0", end="")
-        else:
-            print("1", end="")
-    print("")
